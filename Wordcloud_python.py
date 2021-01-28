4a7b2ebc986fd568ebe21a99d3517fff41f08484
invest41 <70070334+invest41@users.noreply.github.com>
2021-01-28 20:51:19 +0100

    Create Wordcloud_python.py

---
+++ Wordcloud_python.py
@@ -0,0 +1,46 @@
+# Python program to generate WordCloud 
+  
+# importing all necessery modules 
+from wordcloud import WordCloud, STOPWORDS 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+  
+# Reads csv file  
+df = pd.read_csv(r"/content/clean_work.csv", encoding ="latin-1") 
+  
+comment_words = '' 
+stopwords = set(STOPWORDS) 
+  
+# iterate through the csv file 
+for key,val in df.iterrows(): 
+      
+    # typecaste each val to string 
+    val = str(val) 
+  
+    # split the value 
+    tokens = val.split() 
+      
+    # Converts each token into lowercase 
+    co = 0
+    for i in range(len(tokens)):
+        co+=1
+        if co>=5:
+          del tokens[i:]
+          break
+        tokens[i] = tokens[i].lower() 
+      
+    comment_words += " ".join(tokens)+" "
+  
+wordcloud = WordCloud(width = 800, height = 800, 
+                background_color ='white', 
+                stopwords = stopwords, 
+                min_font_size = 10).generate(comment_words) 
+  
+# plot the WordCloud image                        
+plt.figure(figsize = (8, 117), facecolor = None) 
+plt.imshow(wordcloud) 
+plt.axis("off") 
+plt.tight_layout(pad = 0) 
+
+wordcloud.to_file('cloud.png') 
+plt.show() 
